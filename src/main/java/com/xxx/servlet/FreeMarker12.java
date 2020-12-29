2debc10dbf2e04c527e501bafb1a1c2f81b6f5ac
liman <17719302057@qq.com>
2020-12-29 14:45:25 +0800

    FreeMarker的常见指令

---
+++ src/main/java/com/xxx/servlet/FreeMarker12.java
@@ -0,0 +1,23 @@
+package com.xxx.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *  @author: liman
+ *  @Date: 2020/12/22 15:47
+ *  @Description: FreeMarker 常见指令 ：macro指令
+ */
+@WebServlet("/f12")
+public class FreeMarker12 extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        //请求转发跳转到指定的模板页面 template/f01.ftl
+        req.getRequestDispatcher("template/f12.ftl").forward(req, resp);
+    }
+}
